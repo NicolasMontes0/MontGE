4b114f84b2eb0a8b85c1c7c7a23e05dcffa29d33
Nicolas_M <65338886+NicolasMontes0@users.noreply.github.com>
2022-03-29 18:20:15 +0200

    css accueil
    css accueil

--- module/module_Accueil/VueAccueil.php
+++ module/module_Accueil/VueAccueil.php
@@ -3,12 +3,15 @@
 class VueAccueil {
 
     public function __construct() {
-
+        echo '<link rel="stylesheet" type="text/css" href="module/module_Accueil/VueAccueil.css"/>';
     }
 
     public function afficheAccueil() {
         ?>
-        <h1>Bonjour !</h1>
+        <div id="cadre">
+            <h1>Bonjour,</h1>
+            <p>MontGE existe également sur Android et IOS avec la possibilité de scanner les QRCode des pcs de l'iut !</p>
+        </div>
         <?php
     }
 
